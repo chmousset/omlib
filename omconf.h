3bdfba670f9601eeaff171993e4a804ca3bda9c5
Charles-Henri Mousset <charles-henri.mousset@railnova.eu>
2013-06-02 23:21:39 +0200

    [fix] comment spelling

--- omconf.h
+++ omconf.h
@@ -2,12 +2,10 @@
 #ifndef OMLIB_CONF_H__
 #define OMLIB_CONF_H__
 
-/** 4 gieves good results, straight lines are 0.23% too small and half-circle are 0.02% too small.
+/** 4 gives good results, straight lines are 0.23% too small and half-circle are 0.02% too small.
  * As it is only used for speed control, this is largely sufficient.
  */
 #define LOOKUP_OVERSAMPLING		4
-
-
 
 
 #endif
