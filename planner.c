ede434c4ca1c8187e0fa1adcd38e153a932d363e
Charles-Henri Mousset <charles-henri.mousset@railnova.eu>
2013-09-08 11:37:10 +0200

    [fix] added header with licence

--- planner.c
+++ planner.c
@@ -1,3 +1,22 @@
+/**
+ * OMLib, Open Motion Library
+ * 
+ * Copyright (C) 2013 Charles-Henri Mousset
+ * 
+ * This file is part of OMLib.
+ * 
+ * OMLib is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * OMLib is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * 
+ * See the GNU Lesser General Public License for more details. You should have received a copy of the GNU
+ * General Public License along with OMLib. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Authors: Charles-Henri Mousset
+ */
 
 
 #include <stdio.h>
